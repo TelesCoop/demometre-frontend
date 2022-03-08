1a07def38b712d3f1dbd1531c23243d7c5c1f069
Antoine <6711250-Antoine95@users.noreply.gitlab.com>
2022-03-08 13:49:09 +0000

    Feat/ajoute eslint

--- composables/api.ts
+++ composables/api.ts
@@ -1,100 +1,105 @@
 import { NuxtApp } from "nuxt3/dist/app/nuxt"
 import { useLoadingStore } from "~/stores/loadingStore"
-import { User } from "./types"
 
 let base_url = ""
 type MyHeaders = { [key: string]: string }
 
 // local
 if (process.env.NODE_ENV !== "production") {
-    base_url = "http://localhost:8000"
+  base_url = "http://localhost:8000"
 } else {
-    // production server
-    if (process.server) {
-        // server-side rendering
-        base_url = "http://localhost:8000"
-    }
+  // production server
+  if (process.server) {
+    // server-side rendering
+    base_url = "http://localhost:8000"
+  }
 }
 
 const makeLoadingKey = (path: string) => {
-    // camel-case the path : auth/login -> authLogin
-    if (path.endsWith("/")) {
-        path = path.slice(0, -1)
-    }
-    if (path.startsWith("/")) {
-        path = path.slice(1)
-    }
-    let words = path.split("/")
-    words = words.filter(word => !(/^\d+$/.test(word)))
-    for (const [ix, word] of Object.entries(words.slice(1))) {
-        const newWord = word[0].toUpperCase() + word.slice(1)
-        words[parseInt(ix) + 1] = newWord
-    }
-    return words.join("")
+  // camel-case the path : auth/login -> authLogin
+  if (path.endsWith("/")) {
+    path = path.slice(0, -1)
+  }
+  if (path.startsWith("/")) {
+    path = path.slice(1)
+  }
+  let words = path.split("/")
+  words = words.filter((word) => !/^\d+$/.test(word))
+  for (const [ix, word] of Object.entries(words.slice(1))) {
+    const newWord = word[0].toUpperCase() + word.slice(1)
+    words[parseInt(ix) + 1] = newWord
+  }
+  return words.join("")
 }
 
 const getCsrfCookie = (ctx: NuxtApp) => {
-    let cookie: string = ""
-    if (ctx?.ssrContext?.req.headers.cookie) {
-        cookie = ctx?.ssrContext.req.headers.cookie
-    } else if (process.client) {
-        cookie = document.cookie
-    }
-    if (!cookie) {
-        return null
-    }
-    const csfrRow = cookie.split('; ').find(row => row.startsWith('csrftoken='))
-    if (!csfrRow) {
-        return null
-    }
-    return csfrRow.split('=')[1]
+  let cookie = ""
+  if (ctx?.ssrContext?.req.headers.cookie) {
+    cookie = ctx?.ssrContext.req.headers.cookie
+  } else if (process.client) {
+    cookie = document.cookie
+  }
+  if (!cookie) {
+    return null
+  }
+  const csfrRow = cookie.split("; ").find((row) => row.startsWith("csrftoken="))
+  if (!csfrRow) {
+    return null
+  }
+  return csfrRow.split("=")[1]
 }
 
 const getHeaders = (ctx: NuxtApp, includeCsrf = false): MyHeaders => {
-    const headers: MyHeaders = useRequestHeaders(['cookie'])
-    if (includeCsrf) {
-        const csfrToken = getCsrfCookie(ctx)
-        if (csfrToken) {
-            headers["X-CSRFTOKEN"] = csfrToken
-        }
+  const headers: MyHeaders = useRequestHeaders(["cookie"])
+  if (includeCsrf) {
+    const csfrToken = getCsrfCookie(ctx)
+    if (csfrToken) {
+      headers["X-CSRFTOKEN"] = csfrToken
     }
-    return headers
+  }
+  return headers
 }
 
-
-export const BASE_URL = base_url;
-export const BASE_API_URL = BASE_URL + "/api/";
+export const BASE_URL = base_url
+export const BASE_API_URL = BASE_URL + "/api/"
 
 export async function useApiGet<Type>(path: string) {
-    const loadingStore = useLoadingStore()
+  const loadingStore = useLoadingStore()
 
-    const key = makeLoadingKey(path)
-    loadingStore.markLoading(key)
-    const { data, error } = await useAsyncData<Type>(
-        key,
-        (ctx) => $fetch(BASE_API_URL + path, { method: 'GET', credentials: 'include', headers: getHeaders(ctx) })
-    )
-    if (error.value) {
-        loadingStore.markError(key)
-    } else {
-        loadingStore.markDone(key)
-    }
-    return { data, error }
+  const key = makeLoadingKey(path)
+  loadingStore.markLoading(key)
+  const { data, error } = await useAsyncData<Type>(key, (ctx) =>
+    $fetch(BASE_API_URL + path, {
+      method: "GET",
+      credentials: "include",
+      headers: getHeaders(ctx),
+    })
+  )
+  if (error.value) {
+    loadingStore.markError(key)
+  } else {
+    loadingStore.markDone(key)
+  }
+  return { data, error }
 }
 
 export async function useApiPost<Type>(path: string, payload: any = {}) {
-    const loadingStore = useLoadingStore()
+  const loadingStore = useLoadingStore()
 
-    const key = makeLoadingKey(path)
-    loadingStore.markLoading(key)
-    const { data, error } = await useAsyncData<Type>(
-        key,
-        (ctx) => $fetch(BASE_API_URL + path, { method: 'POST', body: payload, credentials: 'include', headers: getHeaders(ctx, true) })
-    )
-    if (error.value) {
-        loadingStore.markError(key)
-    } else {
-        loadingStore.markDone(key)
-    }
-    return { data, error }
+  const key = makeLoadingKey(path)
+  loadingStore.markLoading(key)
+  const { data, error } = await useAsyncData<Type>(key, (ctx) =>
+    $fetch(BASE_API_URL + path, {
+      method: "POST",
+      body: payload,
+      credentials: "include",
+      headers: getHeaders(ctx, true),
+    })
+  )
+  if (error.value) {
+    loadingStore.markError(key)
+  } else {
+    loadingStore.markDone(key)
+  }
+  return { data, error }
 }
