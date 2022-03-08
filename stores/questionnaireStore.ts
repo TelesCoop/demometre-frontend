3eb150888b7d3f55d3ad26ef868375f3b4b53b50
kout95 <antoine.bernier@outlook.fr>
2022-03-08 14:46:04 +0100

    Lint les nouveaux fichiers

--- stores/questionnaireStore.ts
+++ stores/questionnaireStore.ts
@@ -4,18 +4,20 @@ import { useApiGet } from "~~/composables/api"
 
 type QuestionResponse = { items: Question[] }
 
-export const useQuestionnaireStore = defineStore('questionnaire', {
-    state: () => ({
-        questionById: <{ [key: number]: Question }>{}
-    }),
-    actions: {
-        async loadQuestions() {
-            const { data, error } = await useApiGet<QuestionResponse>("cms/questionnaire-questions")
-            if (!error.value) {
-                for (const question of data.value.items) {
-                    this.questionById[question.id] = question
-                }
-            }
+export const useQuestionnaireStore = defineStore("questionnaire", {
+  state: () => ({
+    questionById: <{ [key: number]: Question }>{},
+  }),
+  actions: {
+    async loadQuestions() {
+      const { data, error } = await useApiGet<QuestionResponse>(
+        "cms/questionnaire-questions"
+      )
+      if (!error.value) {
+        for (const question of data.value.items) {
+          this.questionById[question.id] = question
         }
+      }
     },
+  },
 })
