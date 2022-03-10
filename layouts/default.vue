52e093c794084245bc5e805e964f4431008e3ff5
Alice Andres <alice@telescoop.fr>
2022-03-10 10:02:19 +0000

    Configure pre-commit et applique linter

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-        <Navbar />
-        <slot></slot>
-        <Footer />
-    </div>
+  <div>
+    <Navbar />
+    <slot></slot>
+    <Footer />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from '~/stores/userStore'
+import { useUserStore } from "~/stores/userStore"
 
 const userStore = useUserStore()
 
 if (!userStore.email) {
-    // if profile is already got from SSR, do not fetch again
-    userStore.refreshProfile()
+  // if profile is already got from SSR, do not fetch again
+  userStore.refreshProfile()
 }
 </script>
