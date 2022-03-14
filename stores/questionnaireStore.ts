b706dae925898ad3da41c36a7703f722a4ec20a1
Camille Legeron <camille@telescoop.fr>
2022-03-14 12:08:00 +0000

    Retrieve questionnaire data

--- stores/questionnaireStore.ts
+++ stores/questionnaireStore.ts
@@ -1,23 +1,60 @@
-import { defineStore } from "pinia"
-import { Question } from "~/composables/types"
+import { defineStore, mapActions } from "pinia"
+import { Criteria, Marker, Pillar, Question } from "~/composables/types"
 import { useApiGet } from "~~/composables/api"
-
-type QuestionResponse = { items: Question[] }
 
 export const useQuestionnaireStore = defineStore("questionnaire", {
   state: () => ({
+    pillarById: <{ [key: number]: Pillar }>{},
+    markerById: <{ [key: number]: Marker }>{},
+    criteriaById: <{ [key: number]: Criteria }>{},
     questionById: <{ [key: number]: Question }>{},
   }),
   actions: {
-    async loadQuestions() {
-      const { data, error } = await useApiGet<QuestionResponse>(
-        "cms/questionnaire-questions/"
+    async loadPillars() {
+      const { data, error } = await useApiGet<Pillar[]>(
+        "pillars/"
       )
       if (!error.value) {
-        for (const question of data.value.items) {
+        for (const pillar of data.value) {
+          this.pillarById[pillar.id] = pillar
+        }
+      }
+    },
+    async loadMarkers() {
+      const { data, error } = await useApiGet<Marker[]>(
+        "markers/"
+      )
+      if (!error.value) {
+        for (const marker of data.value) {
+          this.markerById[marker.id] = marker
+        }
+      }
+    },
+    async loadCriterias() {
+      const { data, error } = await useApiGet<Criteria[]>(
+        "criterias/"
+      )
+      if (!error.value) {
+        for (const criteria of data.value) {
+          this.criteriaById[criteria.id] = criteria
+        }
+      }
+    },
+    async loadQuestions() {
+      const { data, error } = await useApiGet<Question[]>(
+        "questionnaire-questions/"
+      )
+      if (!error.value) {
+        for (const question of data.value) {
           this.questionById[question.id] = question
         }
       }
     },
+    async loadQuestionnaire() {
+      await this.loadPillars()
+      await this.loadMarkers()
+      await this.loadCriterias()
+      await this.loadQuestions()
+    }
   },
 })
