54e8e49edce8d11efa327a0afdc95c69171f1509
Camille Legeron <camille@telescoop.fr>
2022-03-17 09:22:00 +0000

    Referential visual

--- pages/referentiel.vue
+++ pages/referentiel.vue
@@ -1,64 +1,51 @@
 <template>
-  <div style="margin: auto; max-width: 1200px; padding-top: 100px">
-    <div style="display: flex">
-      <Pillar
-        v-for="name of names"
-        :key="name"
-        :name="name"
-        :active="name === activePillar"
-        style="cursor: pointer"
-        @click="activePillar = name"
+  <div class="container">
+    <div class="columns" style="padding-top: 100px">
+      <div
+        v-for="pillar of questionnaireStore.pillars"
+        :key="pillar.name"
+        class="column"
+      >
+        <Pillar
+          :name="pillar.name"
+          :active="pillar.name === activePillar?.name"
+          style="cursor: pointer"
+          @click="onSelectPillar(pillar)"
+        />
+      </div>
+    </div>
+
+    <div v-if="activePillar">
+      <PillarReferential
+        :pillar="activePillar"
+        :color="colorClass"
+        :markers="markers"
       />
     </div>
-    <h2 class="title is-2">{{ title }}</h2>
-    <Answers v-model="selectedAnswer" :answers="answers" />
-    <div class="mb-6"></div>
-    <p>Selected answer id : {{ selectedAnswer }}</p>
-    <div class="mb-6"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Answer as AnswerType } from "~/composables/types"
-import { wordTitleCase } from "~/utils"
 import { useQuestionnaireStore } from "~/stores/questionnaireStore"
+import { Marker, PillarType } from "~/composables/types"
 
 const questionnaireStore = useQuestionnaireStore()
 
 if (!Object.keys(questionnaireStore.questionById).length) {
-  questionnaireStore.loadQuestionnaire()
+  questionnaireStore.loadQuestionnaireStructure()
 }
 
-const activePillar = ref("représentation")
-const names = ["représentation", "transparence", "participation", "coopération"]
-const title = computed(() => wordTitleCase(activePillar.value))
-const isAnswerSelected = ref(false)
-const selectedAnswer = ref(0)
-const answers: AnswerType[] = [
-  {
-    title: "Réponse A",
-    description: "Une description super longue pour comprendre plus mieux",
-    id: 0,
-  },
-  {
-    title: "Réponse B",
-    description: "Une description super longue pour comprendre plus mieux",
-    id: 1,
-  },
-  {
-    title: "Réponse C",
-    description: "Une description super longue pour comprendre plus mieux",
-    id: 2,
-  },
-  {
-    title: "Réponse D",
-    description: "Une description super longue pour comprendre plus mieux",
-    id: 3,
-  },
-  {
-    title: "Réponse E",
-    description: "Une description super longue pour comprendre plus mieux",
-    id: 4,
-  },
-]
+const activePillar = ref<PillarType>()
+const markers = ref<Marker[]>()
+
+const colorClass = computed(() =>
+  activePillar.value ? PillarParams[activePillar.value.name].color : ""
+)
+
+const onSelectPillar = (pillar) => {
+  activePillar.value = pillar
+  markers.value = activePillar.value?.markerIds.map(
+    (markerId) => questionnaireStore.markerById[markerId]
+  )
+}
 </script>
