8e75d74aee5b3dcd73cf4da870780affbbbc196b
Camille Legeron <camille@telescoop.fr>
2022-03-17 12:47:06 +0000

    Intro page referentiel

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="homepage container">
     <HomepageHomeIntro />
-
-    <div
-      v-for="question of Object.values(questionnaireStore.questionById)"
-      :key="question.id"
-    >
-      {{ question.code }}
-      {{ question.name }}
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useQuestionnaireStore } from "~/stores/questionnaireStore"
-
-const questionnaireStore = useQuestionnaireStore()
-
-if (!Object.keys(questionnaireStore.questionById).length) {
-  questionnaireStore.loadQuestions()
-}
 </script>
