6ab36c9683dbb4062a49cd2e304054aa73cb2458
Alice <alice@telescoop.fr>
2022-03-17 18:19:56 +0100

    [+] open question component

---
+++ components/question-input/open.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="open">
+    <label class="is-size-6bis mb-3 is-block has-text-grey">
+      Réponse ouverte
+    </label>
+    <textarea
+      class="textarea has-text-grey-dark"
+      :class="`has-background-${props.color}-light has-border-${props.color}-dark`"
+      rows="12"
+      placeholder="..."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  color: { type: String, required: true },
+})
+</script>
+
+<style scoped lang="sass">
+.open
+  margin-right: 85px
+  margin-left: 85px
+</style>
