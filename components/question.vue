da47bbfdc52f385304b330dbea7fb8c663cb76df
Alice <alice@telescoop.fr>
2022-03-21 12:04:11 +0100

    [+] pretty question description

--- components/question.vue
+++ components/question.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section">
     <h1 class="title is-3">{{ question?.questionStatement }}</h1>
-    <div>{{ question?.description }}</div>
+    <div><RichText :rich-text="question?.description"></RichText></div>
 
     <!-- all possible inputs -->
     <div class="my-5">
