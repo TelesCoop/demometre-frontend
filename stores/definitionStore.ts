a6ae3a67d81347be36e40177dec079e59aa7bc85
Alice <alice@telescoop.fr>
2022-03-22 17:55:49 +0100

    [+] define and fill definitionStore

---
+++ stores/definitionStore.ts
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia"
+import { useApiGet } from "~~/composables/api"
+import { Definition } from "~/composables/types"
+import { pick } from "~/composables/tools"
+
+type DefinitionResponse = Definition[]
+
+export const useDefinitionStore = defineStore("definition", {
+  state: () => ({
+    definitionById: <{ [key: number]: Definition }>{},
+  }),
+  actions: {
+    async loadDefinitions() {
+      const { data = ref<DefinitionResponse>(), error = ref() } =
+        await useApiGet<DefinitionResponse>("definitions/")
+
+      if (!error.value) {
+        for (const definition of data.value) {
+          this.definitionById[definition.id] = definition
+        }
+      }
+    },
+    async getDefinitions() {
+      if (!Object.keys(this.definitionById).length) await this.loadDefinitions()
+      else console.log("tralala")
+    },
+    definitionsByIdArray(idArray: number[]) {
+      return pick(this.definitionById, idArray)
+    },
+  },
+})
