1df19e0bbcaf46acf58ff2bdf8be7596d3b5a661
Alice <alice@telescoop.fr>
2022-03-25 17:49:52 +0100

    [*] simplify modelWrapper

--- composables/modelWrapper.ts
+++ composables/modelWrapper.ts
@@ -1,11 +1,11 @@
 import { computed, getCurrentInstance } from "vue"
 
-export const useModel = (props, propName) => {
+export const useModel = (propName) => {
   const vm = getCurrentInstance().proxy
 
   return computed({
     get() {
-      return props[propName]
+      return vm.$props[propName]
     },
     set(value) {
       vm.$emit(`update:${propName}`, value)
