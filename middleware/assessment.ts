1d52e77a4019015f8e81fa57b92cdbdefa6d5116
kout95 <antoine.bernier@outlook.fr>
2022-03-29 12:09:56 +0200

    Ajoute le reload

--- middleware/assessment.ts
+++ middleware/assessment.ts
@@ -1,7 +1,12 @@
 import { useAssessmentStore } from "~~/stores/assessmentStore"
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const assessmentStore = useAssessmentStore()
+
+  // Load data if f5
+  if (process.server) {
+    await useAssessmentStore().getAssessment(to.params.assessmentId)
+  }
   if (!assessmentStore.currentAssessment?.initializationDate) {
     useRouter().push("/evaluation/initier")
   }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt3"
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ["@/css/app.sass"],
-  buildModules: ["@pinia/nuxt"],
+  buildModules: ["@pinia/nuxt", "@nuxtjs/moment"],
   vite: {
     css: {
       preprocessorOptions: {
