63f8b052f0aad2d693bda4827c329e9923f5efe7
Camille Legeron <camille@telescoop.fr>
2022-03-31 14:02:35 +0000

    Auth

---
+++ pages/profil.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <PageIntro title="Mon compte" introduction="" />
+    <div class="mt-1">
+      <button class="button is-normal" type="button" @click="logout">
+        Se déconnecter
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from "~~/stores/userStore"
+
+const userStore = useUserStore()
+const logout = () => {
+  userStore.logout()
+}
+</script>
+
+<style scoped lang="sass"></style>
