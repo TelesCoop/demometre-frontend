aef1bce8cf8283bd74413ff79ddf9bf30b779384
Alice <alice@telescoop.fr>
2022-04-01 18:27:20 +0200

    [+] handle reactivity in modelWrapper.ts

--- composables/modelWrapper.ts
+++ composables/modelWrapper.ts
@@ -1,14 +1,26 @@
 import { computed, getCurrentInstance } from "vue"
 
-export const useModel = (propName) => {
+export const useModel = (
+  propName,
+  options: { type?: "object" | "array" } = {}
+) => {
   const vm = getCurrentInstance().proxy
+
+  let valueToSet
+  if (options.type === "object") {
+    valueToSet = (value: object) => {
+      return { ...value }
+    }
+  } else if (options.type === "array") {
+    valueToSet = (value: any[]) => [...value]
+  } else valueToSet = (value) => value
 
   return computed({
     get() {
       return vm.$props[propName]
     },
     set(value) {
-      vm.$emit(`update:${propName}`, value)
+      vm.$emit(`update:${propName}`, valueToSet(value))
     },
   })
 }
