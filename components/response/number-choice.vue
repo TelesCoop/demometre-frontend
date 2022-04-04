ada41e76c67e5c9bd4d9745f82723cde412d5d6e
Alice <alice@telescoop.fr>
2022-04-04 14:34:18 +0200

    [+] input slider

--- components/response/number-choice.vue
+++ components/response/number-choice.vue
@@ -7,13 +7,16 @@
       hide-left-symbol
     >
       <template #right-symbol>
-        <input
-          v-model="answer"
-          type="number"
-          :min="props.bounds.min.value"
-          :max="props.bounds.max.value"
-        />
-        <!-- TODO slider here https://wikiki.github.io/form/slider/ -->
+        <div class="is-flex input-container">
+          <Slider
+            v-model="answer"
+            class="choice-slider"
+            :min="props.bounds.min.value"
+            :max="props.bounds.max.value"
+            :tooltips="false"
+          />
+          <div>{{ props.selected ? answer : "?" }}</div>
+        </div>
       </template>
     </ResponseChoice>
   </label>
@@ -27,6 +30,7 @@ import {
   ResponseChoice as ResponseChoiceType,
 } from "~/composables/types"
 import { useModel } from "~/composables/modelWrapper"
+import Slider from "@vueform/slider"
 
 const props = defineProps({
   category: {
@@ -35,7 +39,7 @@ const props = defineProps({
   },
   modelValue: { type: Number, default: null },
   color: { type: String, required: true },
-  bounds: { type: Object as PropType<QuestionBounds> },
+  bounds: { type: Object as PropType<QuestionBounds>, required: true },
 })
 
 const answer = useModel("modelValue")
@@ -47,3 +51,33 @@ const responseChoice = computed<ResponseChoiceType>(() => {
   }
 })
 </script>
+
+<style src="@vueform/slider/themes/default.css"></style>
+
+<style lang="sass">
+.choice-slider
+  --slider-bg: #FFF
+  --slider-connect-bg: var(--color)
+  --slider-handle-shadow: 0
+  --slider-handle-shadow-active: 0
+  --slider-handle-border: 1px solid var(--color-dark)
+  --slider-handle-width: 18px
+  --slider-handle-height: 18px
+  --slider-handle-bg: var(--color)
+  --slider-height: 12px
+  width: 138px
+
+  // customization not planned by the module
+  .slider-base, .slider-connects
+    border: 1px solid var(--color)
+    box-sizing: content-box
+
+  &.slider-horizontal .slider-handle
+    top: calc(var(--slider-handle-height, 16px)/2*-1 - var(--slider-height, 6px)/2*-1)
+  // end of customization not planned by the module
+
+.input-container
+  width: 168px
+  justify-content: space-between
+  align-items: center
+</style>
