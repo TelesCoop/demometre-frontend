f3255942df7194313b97ab3ab5a566657763385a
Camille <camille@telescoop.fr>
2022-05-21 02:24:14 +0200

    test

--- middleware/user-step.ts
+++ middleware/user-step.ts
@@ -1,17 +1,24 @@
 import { useUserStep } from "~/composables/userStep"
-// import { useUserStore } from "~/stores/userStore"
+import { useUserStore } from "~/stores/userStore"
 
-async function verifyRouting(to, from) {
-  // const userStore = useUserStore()
-  // if (!!to.query.anonymous && !userStore.anonymous.username && !userStore.anonymousRefreshed) {
-  //   await userStore.refreshAnonymous(to.query.anonymous)
-  //   await getParticipationUserData()
-  // }
+async function verifyRouting(to) {
+  if (process.server) {
+    // TODO : make work this code, the redirection is effective before anonymous user is load
+    const userStore = useUserStore()
+    if (
+      !!to.query.anonymous &&
+      !userStore.anonymous.username &&
+      !userStore.anonymousRefreshed
+    ) {
+      await userStore.refreshAnonymous(to.query.anonymous)
+      await getParticipationUserData()
+    }
 
-  const userStep = useUserStep().value
+    const userStep = useUserStep().value
 
-  if (userStep.step !== to.meta.step) {
-    return navigateTo(userStep.url)
+    if (userStep.step !== to.meta.step) {
+      return navigateTo(userStep.url)
+    }
   }
 }
 
