21e40f0acb04677ce4550f411151a87a956398ab
Camille Legeron <camille@telescoop.fr>
2022-07-11 13:35:23 +0000

    Role question statement from backoffice

--- composables/types.ts
+++ composables/types.ts
@@ -396,6 +396,8 @@ export type EvaluationInitPage = {
   initializationValidation: string
 }
 export type EvaluationQuestionnairePage = {
+  roleQuestionTitle: string
+  roleQuestionDescription: string
   startTitle: string
   startText: string
   intermediateStepTitle: string
