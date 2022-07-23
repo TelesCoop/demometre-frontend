d9008a3206bba3632777546ddee8f656cb8de82a
kout95 <antoine.bernier@outlook.fr>
2022-07-23 11:00:13 +0200

    ajoute le chart-percentage

--- components/analytics/percentage-question-chart.vue
+++ components/analytics/percentage-question-chart.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-if="data.ranges.length" class="percentage-chart">
-    {{ data }}
     <div class="percentage-chart-bar" :class="`has-background-${color}-hover`">
       <span>
         <span
@@ -54,14 +53,20 @@
           ></span>
         </template>
       </span>
-
-      <!--      lol-->
-      <!--      <span-->
-      <!--        class="percentage-chart-bar-line-value"-->
-      <!--        :class="`has-text-${color}-hover`"-->
-      <!--        :style="`left: ${valuePosition}%`"-->
-      <!--        >{{ data.value.value }}%</span-->
-      <!--      >-->
+      <span
+        class="percentage-chart-bar-result-line"
+        :class="`has-background-black`"
+        :style="`left: ${valuePosition}%`"
+      ></span>
+      <span
+        class="percentage-chart-bar-result-value"
+        :class="`has-text-black is-size-4${
+          valuePosition < 20 ? ' has-text-right' : ''
+        }`"
+        :style="`left: ${valuePosition}%`"
+        ><strong>{{ data.value.value }}</strong
+        >%</span
+      >
     </div>
   </div>
 </template>
@@ -76,44 +81,50 @@ const props = defineProps({
 const getXPosition = (index: number, length: number): number => {
   return (index / length) * 100
 }
-// const valuePosition = computed(() => {
-//   if (!props.data.ranges.length) {
-//     return 0
-//   }
-//
-//   const rangeIndex = props.data.ranges?.findIndex(
-//     (range) =>
-//       props.data.value.value >= range.lowerBound &&
-//       props.data.value.value <= range.upperBound
-//   )
-//   const currentRange = props.data.ranges?.[rangeIndex]
-//   const percentageBetweenBound =
-//     (props.data.value.value - currentRange.lowerBound) /
-//     (currentRange.upperBound - currentRange.lowerBound)
-//
-//   return (
-//     getXPosition(rangeIndex, props.data.ranges.length) + percentageBetweenBound
-//   )
-// })
+const valuePosition = computed(() => {
+  if (!props.data.ranges.length) {
+    return 0
+  }
+  const value = props.data.value.value
+
+  const rangeIndex = props.data.ranges?.findIndex(
+    (range) => value >= range.lowerBound && value <= range.upperBound
+  )
+  const currentRange = props.data.ranges?.[rangeIndex]
+  const percentageBetweenBound =
+    (value - currentRange.lowerBound) /
+    (currentRange.upperBound - currentRange.lowerBound)
+
+  return (
+    getXPosition(rangeIndex, props.data.ranges.length) +
+    percentageBetweenBound * getXPosition(1, props.data.ranges.length)
+  )
+})
 </script>
 
 <style scoped lang="sass">
 .percentage-chart
+  $result-line-size: 66px
+  $result-line-offset: 15px
   $chart-size: 24px
   $line-size: 2px
+
+  display: flex
+  align-items: center
+  width: 100%
+
   &-bar
     position: relative
     height: $line-size
-    margin-top: #{($chart-size - $line-size) / 2}
-    margin-bottom: #{($chart-size - $line-size) / 2 + 24px} // 24px to add bottom number
+    margin-top: #{$result-line-size - $result-line-offset}
+    margin-bottom: calc(#{($chart-size - $line-size)} / 2 + 24px) // 24px to add bottom number
 
     width: 100%
-    &-line, &-line-value, &-interval-score
+    &-line, &-line-value, &-interval-score, &-result-line, &-result-value
       display: flex
       position: absolute
 
     &-line
-      top: 20%
       width: $line-size
       height: $chart-size
       transform: translate(-50%, -50%)
@@ -130,4 +141,14 @@ const getXPosition = (index: number, length: number): number => {
     &-interval-score
       transform: translate(-50%, 100%)
       bottom: -16px
+
+    &-result-line
+      height: $result-line-size
+      width: $line-size
+      transform: translate(-50%, calc(-50% - $result-line-offset))
+
+    &-result-value
+      transform: translate(-120%, -#{$result-line-size - $result-line-offset})
+      &.has-text-right
+        transform: translate(25%, -#{$result-line-size - $result-line-offset})
 </style>
