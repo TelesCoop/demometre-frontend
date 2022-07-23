bcf3d0be1d9d879010a802549065a3e27f3df0e4
kout95 <antoine.bernier@outlook.fr>
2022-07-23 15:19:09 +0200

    ajoute les titres

--- components/analytics/choice-question-chart.vue
+++ components/analytics/choice-question-chart.vue
@@ -1,60 +1,70 @@
 <template>
   <div class="choice-question-chart">
-    <template v-for="(choice, key) in data.value" :key="key">
-      <div
-        :class="`has-background-${color}-light`"
-        class="choice-question-chart-cell has-text-right"
-      >
-        {{ choice.label }}
-      </div>
-      <div
-        :class="`has-background-${color}-light`"
-        class="choice-question-chart-bar-cell"
-      >
+    <div
+      class="choice-question-chart-grid mb-1"
+      :class="`has-text-${color}-hover`"
+    >
+      <div>Réponse(s)</div>
+      <div>Résultat(s)</div>
+      <div>Totaux</div>
+    </div>
+    <div class="choice-question-chart-grid">
+      <template v-for="(choice, key) in data.value" :key="key">
         <div
-          class="choice-question-chart-bar"
-          :class="`has-background-${color}`"
-          :style="`width: ${getPercentage(choice.value, data.count)}%`"
-        ></div>
-      </div>
-      <div
-        :class="`has-background-${color}-light`"
-        class="choice-question-chart-cell"
-      >
-        <strong>{{ getPercentage(choice.value, data.count) }}</strong
-        >%
-      </div>
-    </template>
-    <template v-for="index in totalSeparator" :key="index">
-      <AnalyticsChoiceQuestionChartLine
-        class="choice-question-chart-line"
-        :class="`is-${color}`"
-        :index="index - 1"
-        :color="color"
-        :total-line="totalSeparator"
-        :full-line-modulo="fullLineModulo"
-        :percentage-of-space-already-taken="percentageOfSpaceAlreadyTaken"
-        :gap-size="gapSize"
-        :percentage-size="percentageSize"
-      >
-      </AnalyticsChoiceQuestionChartLine>
-      <div
-        v-if="(index - 1) % fullLineModulo === 0"
-        class="choice-question-chart-line-number"
-        :class="`has-text-${color}-hover`"
-        :style="
-          getLeftStyle(
-            index - 1,
-            percentageOfSpaceAlreadyTaken,
-            totalSeparator,
-            gapSize,
-            percentageSize
-          )
-        "
-      >
-        {{ (index - 1) * 10 }}%
-      </div>
-    </template>
+          :class="`has-background-${color}-light`"
+          class="choice-question-chart-cell has-text-right"
+        >
+          {{ choice.label }}
+        </div>
+        <div
+          :class="`has-background-${color}-light`"
+          class="choice-question-chart-bar-cell"
+        >
+          <div
+            class="choice-question-chart-bar"
+            :class="`has-background-${color}`"
+            :style="`width: ${getPercentage(choice.value, data.count)}%`"
+          ></div>
+        </div>
+        <div
+          :class="`has-background-${color}-light`"
+          class="choice-question-chart-cell"
+        >
+          <strong>{{ getPercentage(choice.value, data.count) }}</strong
+          >%
+        </div>
+      </template>
+      <template v-for="index in totalSeparator" :key="index">
+        <AnalyticsChoiceQuestionChartLine
+          class="choice-question-chart-line"
+          :class="`is-${color}`"
+          :index="index - 1"
+          :color="color"
+          :total-line="totalSeparator"
+          :full-line-modulo="fullLineModulo"
+          :percentage-of-space-already-taken="percentageOfSpaceAlreadyTaken"
+          :gap-size="gapSize"
+          :percentage-size="percentageSize"
+        >
+        </AnalyticsChoiceQuestionChartLine>
+        <div
+          v-if="(index - 1) % fullLineModulo === 0"
+          class="choice-question-chart-line-number"
+          :class="`has-text-${color}-hover`"
+          :style="
+            getLeftStyle(
+              index - 1,
+              percentageOfSpaceAlreadyTaken,
+              totalSeparator,
+              gapSize,
+              percentageSize
+            )
+          "
+        >
+          {{ (index - 1) * 10 }}%
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -75,10 +85,11 @@ const percentageSize = 80
 
 <style scoped lang="sass">
 .choice-question-chart
-  position: relative
-  display: grid
-  grid-template-columns: 1fr 3fr 80px
-  grid-gap: 1rem
+  &-grid
+    position: relative
+    display: grid
+    grid-template-columns: 1fr 3fr 80px
+    grid-gap: 1rem
 
   &-cell
     padding: 6px
