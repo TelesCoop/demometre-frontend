a8887823a3dddbd1595a97b7f778bf49d0a92129
Camille <camille@telescoop.fr>
2022-09-22 19:26:48 +0200

    Couleur du titre de page reset password

--- pages/nouveau-mdp-confirmation.vue
+++ pages/nouveau-mdp-confirmation.vue
@@ -2,11 +2,13 @@
   <div class="is-flex flex-center">
     <div class="sm-container">
       <div class="my-8 has-text-shade-800">
-        <h1 class="title is-3">Nous avons bien pris en compte votre demande</h1>
+        <h1 class="title is-3 has-text-shade-600">
+          Nous avons bien pris en compte votre demande
+        </h1>
         <p>Veuillez vérifier votre boîte mail et suivre les instructions.</p>
 
-        <NuxtLink to="/login" class="button is-shade-600">
-          Retourner à la connexion</NuxtLink
+        <NuxtLink to="/login" class="button is-shade-600"
+          >Retourner à la connexion</NuxtLink
         >
       </div>
     </div>
