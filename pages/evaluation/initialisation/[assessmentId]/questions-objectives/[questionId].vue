2460ff0c2678b73e2c7e4483690f61c20f0a2e2b
Maxime Bellec <maxime@telescoop.fr>
2024-04-19 14:35:59 +0200

    remove comments

--- pages/evaluation/initialisation/[assessmentId]/questions-objectives/[questionId].vue
+++ pages/evaluation/initialisation/[assessmentId]/questions-objectives/[questionId].vue
@@ -27,13 +27,9 @@ const router = useRouter()
 const questionnaireStore = useQuestionnaireStore()
 const participationStore = useParticipationStore()
 
-console.log("### questionId setup 0")
 const journey = useInitializationJourney()
-console.log("### questionId setup 0.5", { journey })
 const questionId: Ref<number> = ref(+route.params.questionId)
-console.log("### questionId setup", { questionId: questionId.value })
 let pillarName = questionnaireStore.questionById[questionId.value].pillarName
-console.log("### questionId setup 1.01")
 const color = computed<string>(() => PillarParams[pillarName].color)
 
 router.beforeEach((to) => {
@@ -42,13 +38,10 @@ router.beforeEach((to) => {
     pillarName = questionnaireStore.questionById[questionId.value]?.pillarName
   }
 })
-console.log("### questionId setup 1.1")
-console.log("### questionId setup 2", { journey })
 const context: QuestionContextProps = {
   journey,
   questionById: questionnaireStore.questionById,
   responseByQuestionId: participationStore.responseByQuestionnaireQuestionId,
   hasPreviousStep: true,
 }
-console.log("### questionId setup 3")
 </script>
