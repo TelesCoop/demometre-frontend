db82b8d4cfe04f5d781a0d0d9156e17485a05745
Antoine Telescoop <antoine@telescoop.fr>
2024-04-26 15:23:03 +0200

    fix: reload and add loader

---
+++ components/full-page-loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center bg-white h-100-vh is-fullwidth"
+  >
+    <div class="is-flex is-flex-direction-column is-align-items-center">
+      <Logo />
+      <i class="mt-2">Le démometre est en cours de chargement.</i>
+    </div>
+    <Loader
+      color="participation"
+      class="mt-1"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import Logo from "~/components/header/logo.vue"
+</script>
