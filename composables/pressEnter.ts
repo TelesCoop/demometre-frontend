2a736983298752d551613be66341ce080ed00491
Maxime Bellec <maxime@telescoop.fr>
2024-10-16 09:02:04 +0200

    fix: line40: question:  button and prevent sending multiple times the answer when button was pressed

--- composables/pressEnter.ts
+++ composables/pressEnter.ts
@@ -2,6 +2,7 @@ import { onBeforeUnmount, onMounted } from "vue"
 
 export function usePressEnter(submit: () => void, canSubmit?: () => boolean) {
   const onPressEnter = (event: any) => {
+    event.preventDefault()
     // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter" && (!canSubmit || canSubmit())) {
       // Cancel the default action, if needed
